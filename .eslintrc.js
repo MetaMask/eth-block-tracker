bd137d1578d53aa25796e52b3e34204e667dea46
legobt <6wbvkn0j@anonaddy.me>
2023-07-12 07:54:10 +0000

    lint: update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,11 +9,17 @@ module.exports = {
       extends: ['@metamask/eslint-config-typescript'],
       rules: {
         '@typescript-eslint/consistent-type-definitions': 'off',
+        '@typescript-eslint/naming-convention': 'off',
+        '@typescript-eslint/prefer-nullish-coalescing': 'off',
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/unbound-method': 'warn',
         'id-denylist': 'off',
         'id-length': 'off',
         // TODO: Move this to our shared config
         'no-invalid-this': 'off',
+        'no-restricted-syntax': 'off',
         '@typescript-eslint/no-invalid-this': ['error'],
+        '@typescript-eslint/restrict-template-expressions': 'off',
       },
     },
 
