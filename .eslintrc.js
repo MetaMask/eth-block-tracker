80cf48ee500a7efff95a622a23d7aaa2c3691201
legobt <6wbvkn0j@anonaddy.me>
2023-07-12 07:43:30 +0000

    lint: disable subset of new rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,8 @@ module.exports = {
       extends: ['@metamask/eslint-config-typescript'],
       rules: {
         '@typescript-eslint/consistent-type-definitions': 'off',
+        'id-denylist': 'off',
+        'id-length': 'off',
         // TODO: Move this to our shared config
         'no-invalid-this': 'off',
         '@typescript-eslint/no-invalid-this': ['error'],
@@ -21,8 +23,16 @@ module.exports = {
     },
 
     {
-      files: ['*.test.ts', './tests/setupAfterEnv.ts'],
+      files: ['*.test.ts', './tests/*.ts'],
       extends: ['@metamask/eslint-config-jest'],
+      rules: {
+        '@typescript-eslint/naming-convention': 'off',
+        '@typescript-eslint/no-throw-literal': 'off',
+        '@typescript-eslint/restrict-template-expressions': 'off',
+        'id-denylist': 'off',
+        'import/no-nodejs-modules': 'off',
+        'no-restricted-globals': 'off',
+      },
     },
   ],
 
