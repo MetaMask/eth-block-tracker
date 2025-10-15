c72bee25f60cb44cd2ec4ac2847116572f9e9349
Erik Marks <25517051+rekmarks@users.noreply.github.com>
2025-10-15 10:52:08 -0700

    chore: Harmonize lint config, lint deps

---
+++ eslint.config.mjs
@@ -0,0 +1,69 @@
+import base, { createConfig } from '@metamask/eslint-config';
+import jest from '@metamask/eslint-config-jest';
+import nodejs from '@metamask/eslint-config-nodejs';
+import typescript from '@metamask/eslint-config-typescript';
+
+const config = createConfig([
+  {
+    ignores: ['dist/', 'docs/', '.yarn/'],
+  },
+
+  {
+    extends: base,
+
+    languageOptions: {
+      sourceType: 'module',
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: ['./tsconfig.json'],
+      },
+    },
+
+    settings: {
+      'import-x/extensions': ['.js', '.mjs'],
+    },
+  },
+
+  {
+    files: ['**/*.ts'],
+    extends: typescript,
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/prefer-nullish-coalescing': 'off',
+      '@typescript-eslint/no-floating-promises': 'warn',
+      '@typescript-eslint/unbound-method': 'warn',
+      'id-denylist': 'off',
+      'id-length': 'off',
+      // TODO: Move this to our shared config
+      'no-invalid-this': 'off',
+      'no-restricted-syntax': 'off',
+      '@typescript-eslint/no-invalid-this': ['error'],
+      '@typescript-eslint/restrict-template-expressions': 'off',
+    }
+  },
+
+  {
+    files: ['**/*.js', '**/*.cjs'],
+    extends: nodejs,
+
+    languageOptions: {
+      sourceType: 'script',
+    },
+  },
+
+  {
+    files: ['**/*.test.ts', '**/*.test.js'],
+    extends: [jest, nodejs],
+    rules: {
+      '@typescript-eslint/naming-convention': 'off',
+      '@typescript-eslint/no-throw-literal': 'off',
+      '@typescript-eslint/restrict-template-expressions': 'off',
+      'id-denylist': 'off',
+      'import/no-nodejs-modules': 'off',
+      'no-restricted-globals': 'off',
+    }
+  },
+]);
+
+export default config;
